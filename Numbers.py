4750eec0da8cc20dd5a77bcced44c8217c5ca6cb
vaishali72135 <vaishaliprasaad@gmail.com>
2022-12-29 09:42:08 +0530

    Simple numbers

---
+++ Numbers.py
@@ -0,0 +1,54 @@
+def print_numbers_ascending(num):
+    for i in range(1, num + 1):
+        if i == num:
+            print(i, end=" ")
+        else:
+            print(i, end=", "),
+
+print_numbers_ascending(10)
+
+s1 = "TheGoodTheBadTheUgly"
+
+s1_list = list(s1)
+
+print("\n" + "\n " + "To reverse a string")
+for i in range(len(s1_list) -1, -1, -1):
+    print(s1[i], end="")
+
+print("\n")
+print("To swap two variables")
+
+a = 10
+b = 20
+
+print("a: " + str(a) + "  b: " + str(b))
+a, b = b, a
+
+print("a: " + str(a) + "  b: " + str(b))
+
+print("\n")
+print("Fibonacci numbers")
+# 1 2 3 5 8 13 21
+def fibonacci_numbers (num):
+    start = 1
+    mid = 2
+    print (str(start) + ", " + str(mid), end=", ")
+
+    for i in range(num - 2):
+        end = start + mid
+        print(end, end=", ")
+        start = mid
+        mid = end
+
+fibonacci_numbers(7)
+
+print("\n")
+print("factorial of number")
+def factorial(num):
+    temp = num;
+    while num > 1:
+        temp = temp * (num - 1)
+        print(str(temp) + "  num: " + str(num))
+        num = num - 1
+
+factorial(7)
