120e01a6e9b4d575ffbab49bf485419bcb5b7297
vaishali72135 <vaishaliprasaad@gmail.com>
2022-12-29 09:44:18 +0530

    more codes

--- Numbers.py
+++ Numbers.py
@@ -52,3 +52,17 @@ def factorial(num):
         num = num - 1
 
 factorial(7)
+
+
+print("\n")
+for i in range (1, 5):
+    for j in range (1, i + 1):
+        print("*", end="")
+    print("\n", end="")
+
+
+print("\n")
+for x in range (0, 5):
+    for y in range (0, 5-x):
+        print("*", end="")
+    print("\n", end="")
